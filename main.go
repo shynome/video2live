ad930762fa91d57d434658a5265745109d9fa52f
shynome <shynome@gmail.com>
2023-12-19 00:26:33 +0800

    访问 / 路径时重定向到 /_/

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
 	"github.com/shynome/err0/try"
 	_ "github.com/shynome/video2live/migrations"
 )
@@ -12,5 +16,11 @@ func main() {
 	app := pocketbase.New()
 	app.RootCmd.Version = Version
 	initLive(app)
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.GET("/", func(c echo.Context) error {
+			return c.Redirect(http.StatusTemporaryRedirect, "/_/")
+		})
+		return nil
+	})
 	try.To(app.Start())
 }
